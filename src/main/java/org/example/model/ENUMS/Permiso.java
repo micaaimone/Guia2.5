5f77b7860fee4ad6f9fe97580b4a5380090023ad
Micaela Aimone <mica.aimone@gmail.com>
2025-04-01 08:31:03 -0300

    enums

---
+++ src/main/java/org/example/model/ENUMS/Permiso.java
@@ -0,0 +1,5 @@
+package org.example.model.ENUMS;
+
+public enum Permiso {
+    CLIENTE, ADMINISTRADOR, GESTOR;
+}
---
+++ src/main/java/org/example/model/ENUMS/tipoCuenta.java
@@ -0,0 +1,5 @@
+package org.example.model.ENUMS;
+
+public enum tipoCuenta {
+    CAJA_AHORRO, CUENTA_CORRIENTE;
+}
