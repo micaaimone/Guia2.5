7e220908a04f67a6941902cffbe7b950c9da8357
Micaela Aimone <mica.aimone@gmail.com>
2025-04-01 18:54:16 -0300

    main - inicializo ui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.view.UI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UI ui = new UI();
+        ui.menu();
     }
 }
